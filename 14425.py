5083567e928834c636330568596983310078f776
YOMI <balm0116@naver.com>
2022-11-15 15:15:16 +0900

    2022-11-15

--- 14425.py
+++ 14425.py
@@ -1,16 +1,13 @@
 import sys
-input = sys.stdin.readline
+input = sys.stdin.readline # 시간 복잡도가 낮음 input보다
 
 N, M = map(int,input().split())
 li1 = set()
-li2 = []
 for _ in range(N) :
     li1.add(input())
-for _ in range(M) :
-    li2.append(input())
 num = 0
-for st1 in li1 :
-    for st2 in li2 :
-        if st1 == st2 :
-            num += 1
+for _ in range(M) : # 이중 for문은 시간복잡도가 크다.
+    li2 = input()
+    if li2 in li1 :
+        num += 1
 print(num)
