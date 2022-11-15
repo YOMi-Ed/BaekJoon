59317c24303468dcb8cbe7f43419a89bca9edf58
YOMI <balm0116@naver.com>
2022-11-16 00:20:12 +0900

    2022-11-16

--- 1764.py
+++ 1764.py
@@ -2,13 +2,13 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-list_n = set()
+list_n = set() # 집합을 이용해서....
 list_m = set()
 for _ in range(N) :
     list_n.add(input())
 for _ in range(M) :
     list_m.add(input())
-arr = sorted(list(list_n & list_m))
+arr = sorted(list(list_n & list_m)) # 교집합을 찾는다
 print(len(arr))
 
 for i in arr :
