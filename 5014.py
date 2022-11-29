4f37dfa6cc9101bf3aa7fb269a98e82701e12b8b
YOMI <balm0116@naver.com>
2022-11-29 21:06:37 +0900

    bfs

---
+++ 5014.py
@@ -0,0 +1,20 @@
+# 다른 사람 코드 참고함 어떻게 이런 생각을 하지?
+from collections import deque
+
+F, S, G, U, D = map(int, input().split())
+visited = [0] * (F + 1)
+
+def bfs(s):
+    q = deque([s])
+    visited[s] = 1
+    while q:
+        v = q.popleft()
+        for nv in [v + U, v - D]:
+            if 1 <= nv <= F and not visited[nv]:
+                q.append(nv)
+                visited[nv] = visited[v] + 1
+bfs(S)
+if visited[G]:
+    print(visited[G] - 1)
+else:
+    print("use the stairs")
